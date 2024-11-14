6be5be440f7dc6376b9fcd56b082d945880083ac
anixel <45008199+andifg@users.noreply.github.com>
2024-11-14 11:44:47 -0300

    add random counter statistics

--- counter/counter.go
+++ counter/counter.go
@@ -2,6 +2,7 @@ package counter
 
 import (
 	//"fmt"
+	//"errors"
 	log "github.com/sirupsen/logrus"
 	"math/rand/v2"
 )
@@ -10,51 +11,72 @@ type Counter struct {
 	sum int
 }
 
-func (c *Counter) Add(number int) {
+func (c *Counter) Add(number int) CounterOperationReport {
 	log.Debug("add got called")
 	c.sum = c.sum + number
 	log.Debug("New total value", c.sum)
+	return CounterOperationReport{
+		mathOperation: Add,
+		currentSum:    c.sum,
+	}
 }
 
-func (c *Counter) Subtract(number int) {
+func (c *Counter) Subtract(number int) CounterOperationReport {
 	log.Debug("Subtract got called")
 	c.sum = c.sum - number
 	log.Debug("New total value", c.sum)
+	return CounterOperationReport{
+		mathOperation: Subtract,
+		currentSum:    c.sum,
+	}
+
 }
 
-func (c *Counter) Multiply(number int) {
+func (c *Counter) Multiply(number int) CounterOperationReport {
 	log.Debug("Will multiply by ", number)
 	c.sum = c.sum * number
 	log.Debug("New total value", c.sum)
+	return CounterOperationReport{
+		mathOperation: Multiply,
+		currentSum:    c.sum,
+	}
 }
 
-func (c *Counter) Divide(number int) {
+func (c *Counter) Divide(number int) CounterOperationReport {
 	log.Debug("Divide Called")
 	if number == 0 {
 		log.Debug("Would divide by zero, return instead")
-		return
+		return CounterOperationReport{
+			mathOperation: Divide,
+			currentSum:    c.sum,
+		}
 	}
 	c.sum = c.sum * number
 	log.Debug("New total value", c.sum)
+	return CounterOperationReport{
+		mathOperation: Divide,
+		currentSum:    c.sum,
+	}
 }
 
-func (c *Counter) ExecRandom() {
+func (c *Counter) ExecRandom() CounterOperationReport {
 	methodToCall := rand.IntN(4)
 	calcNumber := rand.IntN(10)
 	switch methodToCall {
 	case 0:
 		log.Trace("Will call add method!")
-		c.Add(calcNumber)
+		return c.Add(calcNumber)
 	case 1:
 		log.Trace("Will call subtract method!")
-		c.Subtract(calcNumber)
+		return c.Subtract(calcNumber)
 	case 2:
 		log.Trace("Will call multiply method!")
-		c.Multiply(calcNumber)
+		return c.Multiply(calcNumber)
 	case 3:
 		log.Trace("Will call divide method!")
-		c.Divide(calcNumber)
+		return c.Divide(calcNumber)
 	}
+	panic("Impossible case")
 }
 
 func NewCounter() *Counter {
--- counter/randomCounter.go
+++ counter/randomCounter.go
@@ -1,21 +1,46 @@
 package counter
 
 import (
-	//"fmt"
+	"encoding/json"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"math/rand/v2"
 )
 
 type RandomCounter struct {
-	counter CounterSystem
+	counter   CounterSystem
+	statistic RandomCounterStatistic
+}
+
+type RandomCounterStatistic struct {
+	TotalSum      int `json:"totalSum"`
+	TotalRounds   int `json:"totalRounds"`
+	AddCount      int `json:"addCount"`
+	SubtractCount int `json:"subtractCount"`
+	MultiplyCount int `json:"multiplyCount"`
+	DivideCount   int `json:"divideCount"`
+}
+
+type MathOperation int
+
+const (
+	Add MathOperation = iota
+	Subtract
+	Multiply
+	Divide
+)
+
+type CounterOperationReport struct {
+	mathOperation MathOperation
+	currentSum    int
 }
 
 type CounterSystem interface {
-	Add(number int)
-	Subtract(number int)
-	Multiply(number int)
-	Divide(number int)
-	ExecRandom()
+	Add(number int) CounterOperationReport
+	Subtract(number int) CounterOperationReport
+	Multiply(number int) CounterOperationReport
+	Divide(number int) CounterOperationReport
+	ExecRandom() CounterOperationReport
 }
 
 func (r *RandomCounter) setCounter(counter CounterSystem) {
@@ -27,14 +52,43 @@ func (r *RandomCounter) PlayRandomRounds() {
 		log.Panic("ACHTUNG")
 	}
 	roundsToPlay := rand.IntN(100)
+	log.Info("Will play ", roundsToPlay, " rounds")
 	for roundNumber := range roundsToPlay {
-		log.Info("Round: ", roundNumber)
-		r.counter.ExecRandom()
+		log.Debug("Round: ", roundNumber)
+		r.executeRandomProcessResult()
 	}
+	r.printSummary()
+}
+
+func (r *RandomCounter) executeRandomProcessResult() {
+	res := r.counter.ExecRandom()
+	r.statistic.TotalRounds += 1
+	r.statistic.TotalSum += res.currentSum
+	switch res.mathOperation {
+	case Add:
+		r.statistic.AddCount += 1
+	case Subtract:
+		r.statistic.SubtractCount += 1
+	case Multiply:
+		r.statistic.MultiplyCount += 1
+	case Divide:
+		r.statistic.DivideCount += 1
+	}
+}
+
+func (r *RandomCounter) printSummary() {
+	// log.Info("Counted " ,r.statistic, "add rounds" )
+	out, err := json.MarshalIndent(r.statistic, "", "  ")
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	fmt.Printf(string(out))
 }
 
 func NewRandomCounter(counter CounterSystem) *RandomCounter {
 	r := new(RandomCounter)
+	r.statistic = RandomCounterStatistic{}
 	r.setCounter(counter)
 	return r
 }
